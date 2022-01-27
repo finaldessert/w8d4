98a99ec394406a5cf0ee961218c1d814c52b0e99
Ben <finaldessert@gmail.com>
2022-01-27 15:12:28 -0500

    Complete addNumbers v5

--- callbacks_exercise.js
+++ callbacks_exercise.js
@@ -47,16 +47,29 @@ const rl = readline.createInterface({
 });
 
 
-function addNumbers(sum, numsLeft, completionCallback) {
+function addNumbers(sum=0, numsLeft, completionCallback) {
     if (numsLeft > 0) { 
         rl.question('How about a number?', function (answer) {
-        const response = answer;
-        })
+            const response = answer;
+            newsum = parseInt(response) + sum;
+            console.log(`Thank you your current sum is: ${newsum}`);
+            numsLeft--;
+            console.log(`You have this many tries remaining: ${numsLeft}`);
+
+           addNumbers(newsum, numsLeft--, completionCallback)
+        }) 
+    } else {
+        console.log(`This is your total:`);
+        console.log(completionCallback(newsum)); //why we using completion callback?
+        rl.close();
     }
 }
-// completionCallBack
-//     rl.close();
 
+function completion(sum) {
+    return sum
+}
+
+addNumbers(0, 4, completion)
 
 // rl.question('What do you think of JavaScript? ', function (answer) {
 //     const response = answer;
@@ -69,3 +82,4 @@ function addNumbers(sum, numsLeft, completionCallback) {
 //     console.log(`You said: ${truth}. Thank you for your honesty.`);
 //     rl.close();
 // });
+
