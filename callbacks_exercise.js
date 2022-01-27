ba460eb97c38302f7184399ab678f0fa94af307c
bill-the-dev <billthedev@gmail.com>
2022-01-27 13:51:55 -0500

    Add printTime v2

--- callbacks_exercise.js
+++ callbacks_exercise.js
@@ -1,5 +1,17 @@
 class Clock {
     constructor() {
-       this.date = new Date()
+       let dateStart = new Date();
+       this.hh = dateStart.getHours();
+       this.mm = dateStart.getMinutes();
+       this.ss = dateStart.getSeconds();
     };
+
+    printTime() {
+        console.log(`${this.hh}:${this.mm}:${this.ss}`)
+        // Format the time in HH:MM:SS
+        // Use console.log to print it.
+    }
 };
+
+clock = new Clock();
+clock.printTime();
