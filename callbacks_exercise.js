c3468648240d5124693d2fa6105f851464056ce6
Ben <finaldessert@gmail.com>
2022-01-27 14:18:20 -0500

    Complete clock v3

--- callbacks_exercise.js
+++ callbacks_exercise.js
@@ -10,8 +10,31 @@ class Clock {
         console.log(`${this.hh}:${this.mm}:${this.ss}`)
         // Format the time in HH:MM:SS
         // Use console.log to print it.
-    }
+    };
+
+    _tick() {
+        setTimeout (() => {
+            this.printTime()
+            if (this.ss < 60) {
+                this.ss += 1;
+            } else if (this.ss === 60) {
+                this.ss = 0;
+                this.mm += 1;
+                if (this.mm === 60) {
+                    this.mm = 0;
+                    this.hh += 1;
+                    if (this.hh === 24) {
+                        this.hh = 1;
+                    }
+                }
+            }
+            this._tick()
+        }, 1000);
+        // 1. Increment the time by one second.
+        // 2. Call printTime.
+      };
 };
 
+
 clock = new Clock();
-clock.printTime();
+clock._tick();
