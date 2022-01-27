8ce21a82cdf783236b183c5f979668dddb88db90
bill-the-dev <billthedev@gmail.com>
2022-01-27 16:34:13 -0500

    Complete myBind v6

--- callbacks_exercise.js
+++ callbacks_exercise.js
@@ -40,36 +40,37 @@ class Clock {
 // clock._tick();
 
 
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// });
 
 
-function addNumbers(sum=0, numsLeft, completionCallback) {
-    if (numsLeft > 0) { 
-        rl.question('How about a number?', function (answer) {
-            const response = answer;
-            newsum = parseInt(response) + sum;
-            console.log(`Thank you your current sum is: ${newsum}`);
-            numsLeft--;
-            console.log(`You have this many tries remaining: ${numsLeft}`);
+// function addNumbers(sum=0, numsLeft, completionCallback) {
+//     if (numsLeft > 0) { 
+//         rl.question('How about a number?', function (answer) {
+//             const response = answer;
+//             newsum = parseInt(response) + sum;
+//             console.log(`Thank you your current sum is: ${newsum}`);
+//             numsLeft--;
+//             console.log(`You have this many tries remaining: ${numsLeft}`);
 
-           addNumbers(newsum, numsLeft--, completionCallback)
-        }) 
-    } else {
-        console.log(`This is your total:`);
-        console.log(completionCallback(newsum)); //why we using completion callback?
-        rl.close();
-    }
-}
+//            addNumbers(newsum, numsLeft--, completionCallback)
+//         }) 
+//     } else {
+//         console.log(newsum)
+//         console.log(`This is your total:`);
+//         console.log(completionCallback(newsum)); //why we using completion callback?
+//         rl.close();
+//     }
+// }
 
-function completion(sum) {
-    return sum
-}
+// function completion(sum) {
+//     return sum
+// }
 
-addNumbers(0, 4, completion)
+// addNumbers(0, 4, completion)
 
 // rl.question('What do you think of JavaScript? ', function (answer) {
 //     const response = answer;
@@ -82,4 +83,38 @@ addNumbers(0, 4, completion)
 //     console.log(`You said: ${truth}. Thank you for your honesty.`);
 //     rl.close();
 // });
+
+
+Function.prototype.myBind = function(context) {
+    debugger
+    let that = this
+    return function() {
+        debugger
+        that.call(context)
+    }
+}
+    // return fxn where the context is bound  
+
+
+class Lamp {
+    constructor() {
+        this.name = "a lamp";
+    }
+}
+
+const turnOn = function () {
+    console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+boundTurnOn(); // should say "Turning on a lamp"
+myBoundTurnOn(); // should say "Turning on a lamp"
+
+
 
