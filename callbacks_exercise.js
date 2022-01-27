d7f3cb3345dbf86acd9f1cb94e59ee6accede32a
bill-the-dev <billthedev@gmail.com>
2022-01-27 14:42:08 -0500

    Add addNumbers method

--- callbacks_exercise.js
+++ callbacks_exercise.js
@@ -36,5 +36,36 @@ class Clock {
 };
 
 
-clock = new Clock();
-clock._tick();
+// clock = new Clock();
+// clock._tick();
+
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) { 
+        rl.question('How about a number?', function (answer) {
+        const response = answer;
+        })
+    }
+}
+// completionCallBack
+//     rl.close();
+
+
+// rl.question('What do you think of JavaScript? ', function (answer) {
+//     const response = answer;
+//     console.log(`Thank you for your valuable feedback: ${response}`);
+//     rl.close();
+// });
+
+// rl.question('What do you really think of JavaScript? ', function (answer) {
+//     const truth = answer;
+//     console.log(`You said: ${truth}. Thank you for your honesty.`);
+//     rl.close();
+// });
